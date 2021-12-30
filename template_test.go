f5d97b907df110f75454cca62a072bfb74c1ed23
paul-ri <54000402+paul-ri@users.noreply.github.com>
2021-12-30 10:36:47 +0000

    Support templates hosted on SourceHut and GitLab (e.g. wofi)  (#58)
    * template:Add GetRawBaseURL to support other git hosts than GitHub
    
    The template source from base16-templates-source[0] contains a link to a
    SourceHut repository (e.g. wofi). Only GitHub is supported at the
    moment.
    
    We could add support for user settings to set the raw base url as well
    as the main repository branch (which isn't 'master' by default in most
    new repos), but sane defaults should work for now.
    
    [0]: https://github.com/chriskempson/base16-templates-source
    
    * test:template:Add test for GetRawBaseURL

--- template_test.go
+++ template_test.go
@@ -5,6 +5,54 @@ import (
 	"testing"
 )
 
+func TestBase16TemplateList_GetRawBaseURL(t *testing.T) {
+	type args struct {
+		url string
+		mainBranch string
+	}
+	type result struct {
+		result 	string
+		error	string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    result
+	}{
+		{
+			"sourcehut",
+			args{"https://git.sr.ht/foo/bar.git", "master"},
+			result{"https://git.sr.ht/foo/bar.git/blob/master/", ""},
+		},
+		{
+			"github",
+			args{"https://github.com/foo/bar.git", "master"},
+			result{"https://raw.githubusercontent.com/foo/bar.git/master/", ""},
+		},
+		{
+			"gitlab",
+			args{"https://gitlab.com/foo/bar.git", "master"},
+			result{"https://gitlab.com/foo/bar.git/-/raw/master/", ""},
+		},
+		{
+			"unsupported",
+			args{"https://baz.com/foo/bar.git", "master"},
+			result{"", "git host \"baz.com\" for \"foo/bar.git\" not supported yet"},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := GetRawBaseURL(tt.args.url, tt.args.mainBranch)
+			if got != tt.want.result {
+				t.Errorf("GetRawBaseURL() result = %v, want %v", got, tt.want.result)
+			}
+			if err != nil && err.Error() != tt.want.error  {
+				t.Errorf("GetRawBaseURL() error = '%v', want '%v'", err, tt.want.error)
+			}
+		})
+	}
+}
+
 func TestBase16TemplateList_GetBase16Template(t *testing.T) {
 	type fields struct {
 		templates map[string]string
